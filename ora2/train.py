459ce7230f708f66d624cc1a7fdc6504859a9173
csekok <csekok@gamma.ttk.pte.hu>
2021-09-17 17:00:24 +0200

    2. óra vége

---
+++ ora2/train.py
@@ -0,0 +1,7 @@
+distance = 64  # km
+speed = 48  # km/h
+time_in_hours = distance / speed
+time_in_minutes = time_in_hours * 60
+print("Pécsről Dombóvárra", time_in_minutes, "perc alatt ér a vonat.")
+print("alma"[2:4])
+print("alma"[2:5])
