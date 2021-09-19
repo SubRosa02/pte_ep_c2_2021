6d699c93ab34dedd14f2fe30fb62e92bd31b4d83
csekok <csekok@gamma.ttk.pte.hu>
2021-09-19 09:11:34 +0200

    Clean files

--- ora2/density.py
+++ ora2/density.py
@@ -5,4 +5,3 @@ copper_density = 8.69  # g/cm^3
 lead_mass = lead_volume * lead_density
 copper_mass = copper_volumne * copper_density
 print("Az ólom nehezebb:", lead_mass > copper_mass)
-print(3 != 5)
--- ora2/train.py
+++ ora2/train.py
@@ -3,5 +3,3 @@ speed = 48  # km/h
 time_in_hours = distance / speed
 time_in_minutes = time_in_hours * 60
 print("Pécsről Dombóvárra", time_in_minutes, "perc alatt ér a vonat.")
-print("alma"[2:4])
-print("alma"[2:5])
